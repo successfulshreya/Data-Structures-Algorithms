d2f4ae7651a536cb17103007f8d68c96d35a8d8c
successfulshreya <shreyasahu0112@gmail.com>
2026-09-24 11:58:21 +0530

    dsa

--- 11_2D_Arrays/spiral_jalebi.java
+++ 11_2D_Arrays/spiral_jalebi.java
@@ -5,8 +5,8 @@ public class spiral_jalebi {
         int arr[][] = {
             {1, 2, 3, 4},
             {5, 6, 7, 8},
-            {9, 10, 11, 12},
-            {13, 14, 15, 16}
+            {9,10,11,12},
+            {13,14,15,16}
         };
         int n = arr.length;
         int m = arr[0].length;
